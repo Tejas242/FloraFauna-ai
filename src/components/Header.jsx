61a2200abffbfde69d3347e95cfff878782a4485
Tejas Mahajan <tmahajan09876@gmail.com>
2024-01-28 20:29:42 +0530

    experiments

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+const Header = () => {
+  return (
+    <header className=" py-4 px-6 flex items-center justify-between">
+      <h1 className="text-2xl font-bold">FloraFauna.ai</h1>
+      <nav className="flex items-center">
+        <a href="/" className="px-3">
+          Github
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
