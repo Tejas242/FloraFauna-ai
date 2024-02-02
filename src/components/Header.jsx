30da3b2e552f37b8f032a6a94c549c7ad57ec1d6
KartikLabhshetwar <kartik.labhshetwar@gmail.com>
2024-02-02 22:07:11 +0530

    minor change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
+import { NavLink } from "react-router-dom";
+
 const Header = () => {
     return (
       <header className=" py-4 px-6 flex items-center justify-between bg-slate-950 shadow-lg">
+        <NavLink to={"/"}>
         <h1 className="text-2xl font-bold text-white">FloraFauna.ai</h1>
+        </NavLink>
         <nav className="flex items-center text-white">
-          <a href="/https://github.com/Tejas242/FloraFauna-ai" className="px-3">
+          <button className='w-[10rem] py-[8px] rounded-3xl bg-[#98EC65] hover:bg-[#81E047] mt-[1rem]'>
+          <a href="https://github.com/Tejas242/FloraFauna-ai">
             Github
           </a>
+          </button>
         </nav>
       </header>
     );
