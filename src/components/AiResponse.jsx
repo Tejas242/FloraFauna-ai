6f90bda11a34995553cf7faefd1dee4c57578613
KartikLabhshetwar <kartik.labhshetwar@gmail.com>
2024-02-03 15:41:55 +0530

    final changes done

--- src/components/AiResponse.jsx
+++ src/components/AiResponse.jsx
@@ -42,75 +42,80 @@ const AIResponse = () => {
         </div>
       )}
       {aiResponse && (
-        <div className="backdrop-blur-sm rounded-xl shadow-md p-8 bg-white/10">
+        <div className="backdrop-blur-sm rounded-xl shadow-md p-8 bg-white/10 backdrop-brightness-60">
           <img src={image} className="w-100 h-80  rounded-xl" />
-          <h2 className="text-xl font-semibold mb-4 text-white">Most Likely Species:</h2>
-          <p className="mb-2 text-white">
-            Scientific Name: {aiResponse.most_likely_species.scientific_name}
+          
+          <div className="pt-10">
+          {/* <h2 className="text-xl font-semibold mb-4 text-slate-300 pt-10">Most Likely Species:</h2> */}
+          <p className="mb-2 text-slate-300 text-4xl font-semibold">
+             {aiResponse.most_likely_species.scientific_name}
           </p>
-          <p className="mb-2 text-white">
+          <p className="mb-2 text-slate-300 text-xl font-semibold">
             Common Names:{" "}
             {aiResponse.most_likely_species.common_names.join(", ")}
           </p>
-          <p className="mb-2 text-white">Brief Description:</p>
-          <ul className="list-disc list-inside mb-4 text-white">
+          <p className="mb-2 text-slate-300">Brief Description:</p>
+          <ul className="list-disc list-inside mb-4 text-slate-300">
             {aiResponse.most_likely_species.brief_description.map(
               (desc, index) => (
-                <li key={index} className="text-white">
+                <li key={index} className="text-slate-300">
                   {desc}
                 </li>
               )
             )}
           </ul>
-          <p className=" mb-2 text-white">
+          <p className=" mb-2 text-slate-300">
             Confidence Level:{" "}
             {Math.round(aiResponse.most_likely_species.confidence_level * 100) +
               "%"}
           </p>
-          <div className="mt-4">
-            <hr className="border-t-2 border-gray-300 mx-auto w-full" />
-          </div>
-          <h2 className="text-xl font-semibold mt-8 text-white">Overall Appearance:</h2>
-          <p className="text-gray-700 mb-4 text-white">{aiResponse.overall_appearance}</p>
-          <div className="mt-4 pt-5 ">
-            <hr className="border-t-2 border-gray-300 mx-auto w-full" />
           </div>
 
-          <h2 className="text-xl font-semibold pt-5 text-white">
+          <div className="mt-4 pt-5 ">
+            <hr className="border-t-2 border-gray-500 mx-auto w-full" />
+          </div>
+
+          <h2 className="text-xl font-semibold mt-8 text-slate-300">Overall Appearance:</h2>
+          <p className=" mb-4 text-slate-300">{aiResponse.overall_appearance}</p>
+          <div className="mt-4 pt-5 ">
+            <hr className="border-t-2 border-gray-500 mx-auto w-full" />
+          </div>
+
+          <h2 className="text-xl font-semibold pt-5 text-slate-300">
             Distinguishing Features:
           </h2>
           <ul className="list-disc list-inside mb-4">
             {aiResponse.distinguishing_features.map((desc, index) => (
-              <li key={index} className="text-white">
+              <li key={index} className="text-slate-300">
                 {desc}
               </li>
             ))}
           </ul>
 
           <div className="mt-4 pt-5">
-            <hr className="border-t-2 border-gray-300 mx-auto w-full" />
+            <hr className="border-t-2 border-gray-500 mx-auto w-full" />
           </div>
 
-          <h2 className="text-xl font-semibold pt-5 text-white">Habitat:</h2>
-          <p className="text-white mb-4">{aiResponse.habitat}</p>
+          <h2 className="text-xl font-semibold pt-5 text-slate-300">Habitat:</h2>
+          <p className="text-slate-300 mb-4">{aiResponse.habitat}</p>
 
           <div className="mt-4 pt-5">
-            <hr className="border-t-2 border-gray-300 mx-auto w-full" />
+            <hr className="border-t-2 border-gray-500 mx-auto w-full" />
           </div>
 
-          <h2 className="text-xl font-semibold pt-5 text-white">Geographic Location:</h2>
-          <p className="text-white mb-4">{aiResponse.geographic_location}</p>
+          <h2 className="text-xl font-semibold pt-5 text-slate-300">Geographic Location:</h2>
+          <p className="text-slate-300 mb-4">{aiResponse.geographic_location}</p>
 
           <div className="mt-4 pt-5">
-            <hr className="border-t-2 border-gray-300 mx-auto w-full" />
+            <hr className="border-t-2 border-gray-500 mx-auto w-full" />
           </div>
 
-          <h2 className="text-xl font-semibold pt-5 text-white">
+          <h2 className="text-xl font-semibold pt-5 text-slate-300">
             Links to Additional Resources:
           </h2>
           <ul className="list-disc list-inside">
             {aiResponse.links_to_additional_resources.map((resource, index) => (
-              <li key={index} className="text-white">
+              <li key={index} className="text-slate-300">
                 <a
                   href={resource.link}
                   target="_blank"
