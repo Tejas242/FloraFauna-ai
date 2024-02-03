9e188351ef4b56fafe20ccf51daa4ae46f08e419
KartikLabhshetwar <kartik.labhshetwar@gmail.com>
2024-02-03 14:10:20 +0530

    added blur

--- src/components/AiResponse.jsx
+++ src/components/AiResponse.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef , useState } from "react";
 import { useLocation } from "react-router-dom";
 import ScaleLoader from "react-spinners/ScaleLoader";
-// import bg1 from '../assets/Animal.jpg'
+import bg1 from '../assets/Plant.jpg'
 
 
 
@@ -10,12 +10,12 @@ const AIResponse = () => {
   const { aiResponse, image } = useLocation().state;
   const [loading, setLoading] = useState(true);
 
-  // const style = {
-  //     backgroundImage: `url(${bg1})`,
-  //     backgroundPosition: 'center',
-  //     backgroundSize: 'cover',
-  //     backgroundRepeat: 'no-repeat',
-  //   }
+  const style = {
+      backgroundImage: `url(${bg1})`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+    }
 
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -34,6 +34,7 @@ const AIResponse = () => {
   }
 
   return (
+    <main style={style}>
     <div className="lg:container mx-auto py-8" ref={aiResponseRef}>
       {!aiResponse && (
         <div className="text-center text-gray-600">
@@ -41,27 +42,27 @@ const AIResponse = () => {
         </div>
       )}
       {aiResponse && (
-        <div className="backdrop-blur-sm rounded-xl shadow-md p-8 bg-lime-100">
+        <div className="backdrop-blur-sm rounded-xl shadow-md p-8 bg-white/10">
           <img src={image} className="w-100 h-80  rounded-xl" />
-          <h2 className="text-xl font-semibold mb-4">Most Likely Species:</h2>
-          <p className="text-gray-700 mb-2">
+          <h2 className="text-xl font-semibold mb-4 text-white">Most Likely Species:</h2>
+          <p className="mb-2 text-white">
             Scientific Name: {aiResponse.most_likely_species.scientific_name}
           </p>
-          <p className="text-gray-700 mb-2">
+          <p className="mb-2 text-white">
             Common Names:{" "}
             {aiResponse.most_likely_species.common_names.join(", ")}
           </p>
-          <p className="text-gray-700 mb-2">Brief Description:</p>
-          <ul className="list-disc list-inside mb-4">
+          <p className="mb-2 text-white">Brief Description:</p>
+          <ul className="list-disc list-inside mb-4 text-white">
             {aiResponse.most_likely_species.brief_description.map(
               (desc, index) => (
-                <li key={index} className="text-gray-700">
+                <li key={index} className="text-white">
                   {desc}
                 </li>
               )
             )}
           </ul>
-          <p className="text-gray-700 mb-2">
+          <p className=" mb-2 text-white">
             Confidence Level:{" "}
             {Math.round(aiResponse.most_likely_species.confidence_level * 100) +
               "%"}
@@ -69,18 +70,18 @@ const AIResponse = () => {
           <div className="mt-4">
             <hr className="border-t-2 border-gray-300 mx-auto w-full" />
           </div>
-          <h2 className="text-xl font-semibold mt-8">Overall Appearance:</h2>
-          <p className="text-gray-700 mb-4">{aiResponse.overall_appearance}</p>
-          <div className="mt-4 pt-5">
+          <h2 className="text-xl font-semibold mt-8 text-white">Overall Appearance:</h2>
+          <p className="text-gray-700 mb-4 text-white">{aiResponse.overall_appearance}</p>
+          <div className="mt-4 pt-5 ">
             <hr className="border-t-2 border-gray-300 mx-auto w-full" />
           </div>
 
-          <h2 className="text-xl font-semibold pt-5">
+          <h2 className="text-xl font-semibold pt-5 text-white">
             Distinguishing Features:
           </h2>
           <ul className="list-disc list-inside mb-4">
             {aiResponse.distinguishing_features.map((desc, index) => (
-              <li key={index} className="text-gray-700">
+              <li key={index} className="text-white">
                 {desc}
               </li>
             ))}
@@ -90,26 +91,26 @@ const AIResponse = () => {
             <hr className="border-t-2 border-gray-300 mx-auto w-full" />
           </div>
 
-          <h2 className="text-xl font-semibold pt-5">Habitat:</h2>
-          <p className="text-gray-700 mb-4">{aiResponse.habitat}</p>
+          <h2 className="text-xl font-semibold pt-5 text-white">Habitat:</h2>
+          <p className="text-white mb-4">{aiResponse.habitat}</p>
 
           <div className="mt-4 pt-5">
             <hr className="border-t-2 border-gray-300 mx-auto w-full" />
           </div>
 
-          <h2 className="text-xl font-semibold pt-5">Geographic Location:</h2>
-          <p className="text-gray-700 mb-4">{aiResponse.geographic_location}</p>
+          <h2 className="text-xl font-semibold pt-5 text-white">Geographic Location:</h2>
+          <p className="text-white mb-4">{aiResponse.geographic_location}</p>
 
           <div className="mt-4 pt-5">
             <hr className="border-t-2 border-gray-300 mx-auto w-full" />
           </div>
 
-          <h2 className="text-xl font-semibold pt-5">
+          <h2 className="text-xl font-semibold pt-5 text-white">
             Links to Additional Resources:
           </h2>
           <ul className="list-disc list-inside">
             {aiResponse.links_to_additional_resources.map((resource, index) => (
-              <li key={index} className="text-gray-700">
+              <li key={index} className="text-white">
                 <a
                   href={resource.link}
                   target="_blank"
@@ -124,6 +125,7 @@ const AIResponse = () => {
         </div>
       )}
     </div>
+    </main>
   );
 };
 
